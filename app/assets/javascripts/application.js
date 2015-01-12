31f834296e252e4fc0c4e64fd79a1b4720f4b882
sanket.redkar <sanket.redkar20@gmail.com>
2015-01-13 00:28:36 +0530

    manage admin dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 //= require jquery-ui/datepicker
+//= require ckeditor/init
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require turbolinks
+
 $(document).ready( function(){
   $('.datepicker').datepicker();
 });
--- config/routes.rb
+++ config/routes.rb
@@ -241,7 +241,7 @@ Fedena::Application.routes.draw do
       get :all
       get :search_user_ajax
       get :list_user
-      get :logout
+      delete :logout
     end
     member do
       get :profile
