a25cb75bea218e01a361ea66d4dfb9050be6dba9
sanket.redkar <sanket.redkar20@gmail.com>
2015-03-04 15:31:37 +0530

    added rspec for elactive group

--- spec/models/elective_group_spec.rb
+++ spec/models/elective_group_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
 describe ElectiveGroup do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
+
+  describe 'Associations' do
+    it { is_expected.to belong_to(:batch) }
+    it { is_expected.to have_many(:subjects)}
   end
 
-  it "should create a new instance given valid attributes" do
-    ElectiveGroup.create!(@valid_attributes)
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:batch_id) }
   end
+  # it "should create a new instance given valid attributes" do
+  #   ElectiveGroup.create(:elective_group)
+  # end
 end
